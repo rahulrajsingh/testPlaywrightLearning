a6d9d1cd55f99306ef66907e90bf97791a52d15e
rahulrajsingh <singhrahulraj97@gmail.com>
2022-07-17 00:33:09 +0530

    Update E2E.login.spec.ts

--- tests/E2E/E2E.login.spec.ts
+++ tests/E2E/E2E.login.spec.ts
@@ -22,6 +22,7 @@ test.describe("Login/logout flow", () =>{
 
     })
 
+    //testing
     test("Valid Login scenario with logout", async ( {page}) => {
 
         const signinbutton =  await page.locator("#signin_button");
