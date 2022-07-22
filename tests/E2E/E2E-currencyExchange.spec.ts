7442fa2c3653b5fd55ecca3a555be1fc7aa6c79e
Rahul <singhrahulraj97@gmail.com>
2022-07-22 21:07:00 +0530

    added currency exchange test

--- tests/E2E/E2E-currencyExchange.spec.ts
+++ tests/E2E/E2E-currencyExchange.spec.ts
@@ -1,6 +1,41 @@
 import {test , expect} from '@playwright/test'
 
-test.describe("", () => {
+test.describe("currency purchase", () => {
 
     
+    test.beforeEach(async ( {page}) => {
+        
+        page.goto("http://zero.webappsecurity.com/index.html");
+        await page.click("#signin_button");
+        await page.type("#user_login", "username");
+        await page.type("#user_password", "password");
+        await page.click("input.btn-primary");
+        await page.goto("http://zero.webappsecurity.com/index.html");
+
+    })
+
+    test("currency xchange",async ( {page}) => {
+
+        await page.click("#onlineBankingMenu");
+        await expect(page).toHaveURL("http://zero.webappsecurity.com/online-banking.html");
+
+        await page.click("#pay_bills_link");
+        await expect(page).toHaveURL("http://zero.webappsecurity.com/bank/pay-bills.html");
+
+        await page.click("a[href*='tabs-3']");
+        await page.selectOption("#pc_currency","AUD");
+        await page.type("#pc_amount", "100");
+        await page.click("#pc_inDollars_false");
+        await page.click("#pc_calculate_costs");
+
+        await page.click("#purchase_cash");
+
+        const crncyPurchasedMsg = page.locator("#alert_content");
+        await expect(crncyPurchasedMsg).toHaveText("Foreign currency cash was successfully purchased.");
+
+
+
+
+    })
+
 })
